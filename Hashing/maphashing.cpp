77de5f21eaefe30f7f4e0f609daf12ea62ea5cf0
Rajarshi Sarkar <rajarshisarkar26@gmail.com>
2026-04-30 21:57:58 +0530

    map hashing

---
+++ Hashing/maphashing.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    map<int, int> mpp;
+    for(int i=0; i<n; i++){
+        mpp[arr[i]]++;
+    }
+
+    int q;
+    cin>>q;
+    while(q--){
+        int number;
+        cin>>number;
+        cout<<mpp[number]<<endl;
+    }
+    return 0;
+}
